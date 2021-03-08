976595b6049efac16b7900d85df718d5854d00ff
kikiAgustin <kikiagustin62@gmail.com>
2021-03-08 15:27:57 +0700

    tambah halaman

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const formList = document.querySelector("#form-list");
 const tulisList = document.querySelector("#tulis-list");
 const listKegiatan = document.querySelector("#list-kegiatan");
+const cariList = document.querySelector("#cari-list");
+const hapusSemuaList = document.querySelector("#hapus-semua-list");
 
 formList.addEventListener("submit", createList);
 listKegiatan.addEventListener("click", deleteList);
+cariList.addEventListener("keyup", pencarianList);
+hapusSemuaList.addEventListener("click", hapusList);
 
 function createList(e) {
     e.preventDefault();
@@ -11,7 +15,7 @@ function createList(e) {
     if (tulisList.value) {
         // Memebuat Elemetn Li
         const li = document.createElement("li");
-        li.className = "list-group-item";
+        li.className = "list-group-item todo-item";
         li.appendChild(document.createTextNode(tulisList.value));
 
         // Membuat Element Delete
@@ -43,6 +47,7 @@ function createList(e) {
 
         // Menambahkan elemen Li ke UL
         listKegiatan.appendChild(li);
+        addTodoLocalStorage(tulisList.value);
         tulisList.value = "";
 
     } else {
@@ -70,4 +75,48 @@ function deleteList(e) {
             element.remove();
         }
     }
+}
+
+function pencarianList(e) {
+    const filterText = e.target.value.toLowerCase();
+    let todoItems = document.querySelectorAll(".todo-item");
+
+    todoItems.forEach((item) => {
+        const itemText = item.firstChild.textContent.toLowerCase();
+
+        if (itemText.indexOf(filterText) != -1) {
+            item.setAttribute("style", "display: block;");
+        } else {
+            item.setAttribute("style", "display: none !important");
+        }
+
+    });
+}
+
+function hapusList() {
+    listKegiatan.innerHTML = "";
+}
+
+function getElementFromLocalStorage() {
+
+    let todos;
+
+    if (localStorage.getItem("todos") == null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    return todos;
+
+}
+
+function addTodoLocalStorage(todoInputValue) {
+
+    let todos = getElementFromLocalStorage();
+
+    todos.push(todoInputValue);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
 }
